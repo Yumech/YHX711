a2fa5ec3b094a03d82f5430b802f6d05a722b0e9
Yumech <v0ltreg@gmail.com>
2016-10-25 02:46:30 +0800

    Add files via upload

---
+++ Yhx711.ino
@@ -0,0 +1,34 @@
+
+
+// YHx711.DOUT - pin 6
+// YHx711.SCK - pin 7
+
+#include "Yhx711.h"
+
+YHx711 adc (6, 7);
+long tare1 = 0;
+long tare2 = 0;
+void setup(){
+  Serial.begin(9600);
+  delay(200);
+  adc.setMode(3);
+  delay(200);
+  tare1 = adc.averageValue(80);
+  adc.setMode(2);
+  delay(200);
+  tare2 = adc.averageValue(80); 
+}
+
+
+void loop()
+{
+  //delay(1000);
+  adc.setMode(3);
+  Serial.print("Channel A:");
+   Serial.println((adc.averageValue(32)- tare1)/973.3);
+  adc.setMode(2);
+  Serial.print("Channel B:");
+  Serial.println((adc.averageValue(32)- tare2)/973.3);
+
+  
+}
